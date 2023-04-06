b9add7dae48e2245c02f0b84d72ab781fe4b9dc5
austin <austin@itri.org.tw>
2023-04-06 22:04:52 +0800

    add rest api and first view

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const mongoose = require("mongoose");
 const Student = require("./models/student");
@@ -7,6 +8,7 @@ require("dotenv").config();
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 mongoose
   .connect(process.env.MOGODB_URL)
@@ -20,18 +22,53 @@ mongoose
 app.get("/students", async (req, res) => {
   try {
     let studentData = await Student.find({}).exec();
-    res.send(studentData);
+    //return res.send(studentData);
+    return res.render("students", { studentData });
   } catch (error) {
-    res.status(500).send(error);
+    return res.status(500).send(error);
+  }
+});
+
+app.get("/students/:_id", async (req, res) => {
+  let { _id } = req.params;
+  try {
+    let foundStudent = await Student.findOne({ _id }).exec();
+    return res.send(foundStudent);
+  } catch (e) {
+    return res.status(500).send("尋找資料時發生錯誤。。。");
   }
 });
 
 app.post("/students", async (req, res) => {
   try {
-    let studentData = await Student.find({}).exec();
-    res.send(studentData);
-  } catch (error) {
-    res.status(500).send(error);
+    let { name, age, major, merit, other } = req.body;
+    let newStudent = new Student({
+      name,
+      age,
+      major,
+      scholarship: {
+        merit,
+        other,
+      },
+    });
+    let savedStudent = await newStudent.save();
+    return res.send({
+      msg: "資料儲存成功",
+      data: savedStudent,
+    });
+  } catch (e) {
+    return res.status(500).send("儲存失敗");
+  }
+});
+
+app.delete("/students/:_id", async (req, res) => {
+  try {
+    let { _id } = req.params;
+    let deleteResult = await Student.deleteOne({ _id });
+    return res.send(deleteResult);
+  } catch (e) {
+    console.log(e);
+    return res.status(500).send("無法刪除學生資料");
   }
 });
 
--- models/student.js
+++ models/student.js
@@ -3,7 +3,15 @@ const { Schema } = mongoose;
 
 const studentSchema = new Schema({
   name: { type: String, require: true, minlength: 2 },
-  age: { type: Number, default: 18, max: [80, "有點太老了喔"] },
+  age: {
+    type: Number,
+    default: 18,
+    max: [80, "有點太老了喔"],
+    min: [0, "沒人是0歲以下"],
+  },
+  major: {
+    type: String,
+  },
   scholarship: {
     merit: { type: Number, min: 0, max: [5000, "給太多了"], default: 0 },
     other: { type: Number, min: 0, default: 0 },
